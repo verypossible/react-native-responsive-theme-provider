f3876fec625c129f824e84a4650c83169d7064c8
Eric Bower <neurosnap@gmail.com>
2019-01-20 23:20:05 -0500

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# react-native-theme-provider
+
+## Requirements
+
+* node
+
+## Test
+
+```bash
+yarn
+yarn test
+```
+
+## Build
+
+```bash
+yarn build
+```
---
+++ index.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { Dimensions, ScaledSize } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+export interface ThemeDimensionProps {
+  window: ScaledSize;
+  screen: ScaledSize;
+}
+
+interface IProps<T = any> {
+  theme: T;
+  children: React.ReactNode;
+}
+
+interface IState {
+  window: ScaledSize;
+  screen: ScaledSize;
+}
+
+export default class ReactNativeThemeProvider<T> extends React.Component<
+  IProps<T>,
+  IState
+> {
+  state = {
+    window: Dimensions.get('window'),
+    screen: Dimensions.get('screen'),
+  };
+
+  onChange = ({
+    window,
+    screen,
+  }: {
+    window: ScaledSize;
+    screen: ScaledSize;
+  }) => {
+    this.setState({
+      screen,
+      window,
+    });
+  };
+
+  componentWillMount() {
+    Dimensions.addEventListener('change', this.onChange);
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener('change', this.onChange);
+  }
+
+  render() {
+    const { children, theme } = this.props;
+    const { window, screen } = this.state;
+    const nativeTheme = {
+      ...theme,
+      window,
+      screen,
+    };
+
+    return (
+      <ThemeProvider theme={nativeTheme}>
+        {React.Children.only(children)}
+      </ThemeProvider>
+    );
+  }
+}
